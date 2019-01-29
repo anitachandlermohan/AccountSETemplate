16d6bdb03886e68c9da986cdf3f52be93ec30436
Admin <Admin@STUDENT26>
2019-01-29 15:59:20 +0000

    add account domain and repository functionality

--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -1,32 +1,64 @@
 package com.qa.persistence.repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.qa.persistence.domain.Account;
 
-public class AccountMapRepository implements AccountRepository{
-	
-	Map<Long, Account> account = new HashMap<>();
+import util.JSONUtil;
 
+public class AccountMapRepository implements AccountRepository{
+	JSONUtil jsonutil = new JSONUtil();
+	Map<Long, Account> accountMap = new HashMap<>();
+	
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		String accounts = "";
+		for(Long accountNumber : accountMap.keySet()) {
+			accounts += accountMap.get(accountNumber).toString();
+			 accounts += "\n";
+		}
+		return accounts;
 	}
 
 	public String createAccount(String account) {
-		// TODO Auto-generated method stub
-		return null;
+//		jsonutil.getObjectForJSON(account, accountMap);
+		
+		Account acc = jsonutil.getObjectForJSON(account, Account.class);
+		
+		accountMap.put(acc.getAccountNumber(), acc); 
+		
+		
+		return "WELL DONE";
 	}
 
 	public String deleteAccount(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+		accountMap.remove(id);
+		return "removed " + id;
+		}
+		catch (Exception idNotFound){
+			return "No account with this ID";
+		}
 	}
 
 	public String updateAccount(Long id, String account) {
-		// TODO Auto-generated method stub
-		return null;
+		Account acc = jsonutil.getObjectForJSON(account,Account.class);
+		accountMap.put(id, acc);
+		return "updated " + id;
+		
+	}
+	public Account getAccountByID(Long id) {
+		
+		Account account = accountMap.get(id);
+		return account;
+		
+		
+	}
+	
+	public int getRepoSize() {
+		return accountMap.size();
 	}
 
 }
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.qa.MapTests;
 
 import static org.junit.Assert.*;
 
+import org.json.simple.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,35 +10,78 @@ import org.junit.Test;
 import com.qa.persistence.domain.Account;
 import com.qa.persistence.repository.AccountMapRepository;
 
-public class AccountServiceTest {
+import util.JSONUtil;
 
-	@Before
+public class AccountServiceTest {
+	AccountMapRepository repo ;
+	JSONUtil jsonutil = new JSONUtil();
+	Account catdogsaccount = new Account("Cat", "Dog", 1234L);
+	String catdogAccountString = jsonutil.getJSONForObject(catdogsaccount);
+	Account edsaccount = new Account("Ed", "Eddy", 12345L);
+	String edsAccountString = jsonutil.getJSONForObject(edsaccount);
+
+	@Before 
 	public void setup() {
+		
+		repo = new AccountMapRepository();
+		
+		
+		
 	
 	}
 	
 	@Test
 	public void addAccountTest() {
 		
+	
+		repo.createAccount(catdogAccountString);
+		Account account = repo.getAccountByID(catdogsaccount.getAccountNumber());
+		assertEquals("account not added", account.getAccountNumber(), catdogsaccount.getAccountNumber());
+		
+		
+		
 	}
 	
 	@Test
 	public void add2AccountTest() {
 		
+
+		repo.createAccount(catdogAccountString);
+		repo.createAccount(edsAccountString);
+		assertEquals("accounts not added", 2, repo.getRepoSize());
 	}
 
 	@Test
 	public void removeAccountTest() {
+		repo.createAccount(edsAccountString);
+		repo.deleteAccount(edsaccount.getAccountNumber());
+		assertEquals("account not removed", repo.getRepoSize(), 0);
 		
+		
+		 
 	}
 	
 	@Test
 	public void remove2AccountTest() {
 		
+		repo.createAccount(catdogAccountString);
+		repo.createAccount(edsAccountString);
+		repo.deleteAccount(catdogsaccount.getAccountNumber());
+		repo.deleteAccount(edsaccount.getAccountNumber());
+		assertEquals("accounts not removed", 0, repo.getRepoSize());
+		
+		
 	}
 	
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
+		
+		repo.createAccount(catdogAccountString);
+		repo.createAccount(edsAccountString);
+		repo.deleteAccount(catdogsaccount.getAccountNumber());
+		repo.deleteAccount(edsaccount.getAccountNumber());
+		repo.deleteAccount(2234L);
+		assertEquals("accounts not removed", 0, repo.getRepoSize());
 		
 	}
 	
