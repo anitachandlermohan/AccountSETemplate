9400b245c703fad112f6bf46971a788a2335ec9b
anitachandlermohan <anitachandlermohan@gmail.com>
2019-01-31 10:31:53 +0000

    added database persistence class

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,11 +1,21 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 
 public class Account{
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column
 	private String firstName;
+	@Column
 	private String lastName;
+	@Column
 	private Long accountNumber;
 	
 	public Account(String firstname, String lastname, Long accountNumber) {
